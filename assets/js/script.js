8c6878e3c198b5614a13d6ea15c53867d7057acd
MihaiElisei <mihai.m.elisei@gmail.com>
2022-06-24 19:51:29 +0000

    add function that change players names in players sections and in score box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 const newGameBtn = document.querySelector('.main_buttons-new');
 const instructionsBtn = document.querySelector('.main_buttons-instructions');
 const submitButton = document.getElementById('submit');
+const firstPlayerName = document.querySelector('.game_first-player--name');
+const secondPlayerName = document.querySelector('.game_second-player--name');
+
 // function to prevent default behaviour of the form
 function submitForm(event){
     event.preventDefault();
     submitButton.addEventListener('click', function(){
-        document.querySelector('.players-form').classList.add('hidden');
-        document.querySelector('.game').classList.remove('hidden');
+        document.querySelector('.main_form').classList.add('hidden');
+        document.querySelector('.games_section').classList.remove('hidden');
         document.querySelector('.footer').classList.add('hidden');
     })
+    playersNames();
+}
+
+// replace players names with form inputs
+function playersNames(){
+    const firstPlayer = document.getElementById('fplayer').value;
+    const secondPlayer = document.getElementById('splayer').value;
+    const firstPlayerNameScoreBox = document.querySelector('.score-box-fplayer');
+    const secondPlayerNameScoreBox= document.querySelector('.score-box-splayer');
+    firstPlayerName.innerHTML = firstPlayer;
+    secondPlayerName.innerHTML = secondPlayer;
+    firstPlayerNameScoreBox.innerHTML = firstPlayer + ":";
+    secondPlayerNameScoreBox.innerHTML = secondPlayer + ":";
 }
 
 // hide new game and instruction buttons and display form section
