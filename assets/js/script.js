14cc733eec45ea388786de1106d14f566da36402
MihaiElisei <mihai.m.elisei@gmail.com>
2022-06-24 20:09:15 +0000

    add function for rock paper scissors game and add event listener for start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ const instructionsBtn = document.querySelector('.main_buttons-instructions');
 const submitButton = document.getElementById('submit');
 const firstPlayerName = document.querySelector('.game_first-player--name');
 const secondPlayerName = document.querySelector('.game_second-player--name');
+const startGame = document.querySelector('.game_mid-start');
 
 // function to prevent default behaviour of the form
 function submitForm(event){
@@ -27,10 +28,77 @@ function playersNames(){
     secondPlayerNameScoreBox.innerHTML = secondPlayer + ":";
 }
 
+// play rock paper scissors to decide who start first
+function choseWhoStart(){
+    const firstPlayerName = document.getElementById('fplayer').value;
+    const secondPlayerName = document.getElementById('splayer').value;
+    // array with options
+    let options = ["rock", "paper", "scissors"];
+    // create 1 random num beetwen 0 and 2 for each player
+    let randomNumFirstPlayer = Math.floor(Math.random() * 3);
+    let randomNumSecondPlayer = Math.floor(Math.random() * 3);
+    // select a element from array with the assigned num
+    let firstPlayerCoice = options[randomNumFirstPlayer];
+    let secondPlayerChoice = options[randomNumSecondPlayer];
+    // display choice image
+    let playerOneImage = document.querySelector('.rps_game-fplayer');
+    let playerTwoImage = document.querySelector('.rps_game-splayer');
+    let whoStart = document.querySelector('.rps_game-winner');
+    let firstPlayer;
+    let secondPlayer;
+    // decide who wins
+    if(firstPlayerCoice === "rock" && secondPlayerChoice === "scissors"){
+        playerOneImage.style.backgroundImage = "url(assets/images/rock.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/scissors.png)"
+        firstPlayer = "win";
+        secondPlayer = "lose";
+    }else if(firstPlayerCoice === "scissors" && secondPlayerChoice === "paper"){
+        playerOneImage.style.backgroundImage = "url(assets/images/scissors.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/paper.png)"
+        firstPlayer = "win";
+        secondPlayer = "lose";
+    }else if(firstPlayerCoice === "paper" && secondPlayerChoice === "rock"){
+        playerOneImage.style.backgroundImage = "url(assets/images/paper.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/rock.png)"
+        firstPlayer = "win";
+        secondPlayer = "lose";
+    }else if(firstPlayerCoice === "scissors" && secondPlayerChoice === "rock"){
+        playerOneImage.style.backgroundImage = "url(assets/images/scissors.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/rock.png)"
+        firstPlayer = "lose";
+        secondPlayer = "win";
+    }else if(firstPlayerCoice === "paper" && secondPlayerChoice === "scissors"){
+        playerOneImage.style.backgroundImage = "url(assets/images/paper.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/scissors.png)"
+        firstPlayer = "lose";
+        secondPlayer = "win";
+    }else if(firstPlayerCoice === "rock" && secondPlayerChoice === "paper"){
+        playerOneImage.style.backgroundImage = "url(assets/images/rock.png)"
+        playerTwoImage.style.backgroundImage = "url(assets/images/paper.png)"
+        firstPlayer = "lose";
+        secondPlayer = "win";
+    // reply the game if it`s a tie
+    }else if(firstPlayerCoice === secondPlayerChoice){
+        choseWhoStart();
+    }
+    console.log(firstPlayerCoice, secondPlayerChoice)
+    // Change the winner name and display the winner attack button
+    if(firstPlayer === 'win'){
+        whoStart.innerHTML = firstPlayerName;
+        document.querySelector('.game_first-player--attack').classList.remove('hidden');
+    }else if (secondPlayer === 'win'){
+        whoStart.innerHTML = secondPlayerName;
+        document.querySelector('.game_second-player--attack').classList.remove('hidden');
+    }
+    document.querySelector('.rps_game').classList.remove('hidden');
+
+} 
+
 // hide new game and instruction buttons and display form section
 newGameBtn.addEventListener('click', function(){
     newGameBtn.classList.add('hidden');
     instructionsBtn.classList.add('hidden');
+    document.querySelector('.main_title').classList.add('hidden')
     document.querySelector('.main_form').classList.remove('hidden');
 });
 // open instructions modal
@@ -46,4 +114,6 @@ document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !document.querySelector('.main_instructions').classList.contains('hidden')) {
         document.querySelector('.main_instructions').classList.add('hidden');
     }
-  });
+});
+// event listener for start game
+startGame.addEventListener('click', choseWhoStart);
