128dcd656b99945ceb242739bc10b49226e53426
MihaiElisei <mihai.m.elisei@gmail.com>
2022-06-24 19:34:55 +0000

    add event listener for instructions modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ newGameBtn.addEventListener('click', function(){
     instructionsBtn.classList.add('hidden');
     document.querySelector('.main_form').classList.remove('hidden');
 });
+// open instructions modal
+instructionsBtn.addEventListener('click', function(){
+    document.querySelector('.main_instructions').classList.remove('hidden');
+})
+// close instructions modal
+document.querySelector('.main_instructions-close').addEventListener('click', function(){
+    document.querySelector('.main_instructions').classList.add('hidden');
+})
+// event listener for closing instructions modal when press esc key
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !document.querySelector('.main_instructions').classList.contains('hidden')) {
+        document.querySelector('.main_instructions').classList.add('hidden');
+    }
+  });
